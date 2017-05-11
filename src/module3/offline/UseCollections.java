78827325e0aca7aa681d82a632a6f1e3997b80c5
Pavlo Baranovskyi <baranovskiy.p.p@gmail.com>
2017-05-11 20:37:45 +0300

    module 3

---
+++ src/module3/offline/UseCollections.java
@@ -0,0 +1,19 @@
+package module3.offline;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by citsym on 11.05.17.
+ */
+public class UseCollections {
+
+    public static void main(String[] args) {
+
+        List someOterList = new LinkedList();
+        String urlOfTask = "https://drive.google.com/open?id=0B87Evh6MifEGSktmcTFBTHFHZnc";
+
+        List list = new ArrayList(someOterList);
+    }
+}
